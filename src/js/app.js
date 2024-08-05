b0e5db9d9530e897e56f1718380599dc2d01c745
lina sol <soldatenko_angelina@mail.ru>
2024-08-05 20:47:05 +0400

    edited

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 export function changeHole() {
     const activeHole = document.querySelector(".active-hole");
-    activeHole[0].classList.remove("active-hole");
+    activeHole.classList.remove("active-hole");
     const index = Math.floor(1 + Math.random() * 16);
     const newActive = document.getElementById(`hole${index}`);
     newActive.classList.add("active-hole")
